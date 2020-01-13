edc3251651661da7c4e2d324a174350ba6890f0a
unknown <nancyhuchen@gmail.com>
2020-01-12 20:05:24 -0600

    add GUI

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 #include "main.h"
-
+#include "config.hpp"
+#include "gui.h"
 /**
  * A callback function for LLEMU's center button.
  *
  * When this callback is fired, it will toggle line 2 of the LCD text between
  * "I was pressed!" and nothing.
  */
-
  pros::Controller master(pros::E_CONTROLLER_MASTER);
  pros::Motor leftfront  (1, pros::E_MOTOR_GEARSET_18, true, pros::E_MOTOR_ENCODER_DEGREES);
  pros::Motor leftback   (2, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
@@ -15,16 +15,8 @@
  pros::Motor clawleft   (8, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
  pros::Motor clawright  (9, pros::E_MOTOR_GEARSET_18, true, pros::E_MOTOR_ENCODER_DEGREES);
  pros::Motor lift       (10, pros::E_MOTOR_GEARSET_18, true, pros::E_MOTOR_ENCODER_DEGREES);
- pros::Motor stacker    (11, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
-void on_center_button() {
-	static bool pressed = false;
-	pressed = !pressed;
-	if (pressed) {
-		pros::lcd::set_text(2, "I was pressed!");
-	} else {
-		pros::lcd::clear_line(2);
-	}
-}
+ pros::Motor stacker    (20, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
+
 
 /**
  * Runs initialization code. This occurs as soon as the program is started.
@@ -33,10 +25,7 @@ void on_center_button() {
  * to keep execution time for this mode under a few seconds.
  */
 void initialize() {
-	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
-
-	pros::lcd::register_btn1_cb(on_center_button);
+  gui();
 }
 
 /**
@@ -70,31 +59,245 @@ void competition_initialize() {}
  */
 void autonomous() {
 
-	leftfront.move_velocity(100);
-	leftback.move_velocity(100);
-	rightfront.move_velocity(100);
-	rightback.move_velocity(100);
-	pros::delay(2000);
+  redblue side=red;
+  frontback isfront=front;
 
-	clawleft.move_velocity(-100);
-	clawright.move_velocity(-100);
-	clawleft.move_velocity(0);
-	clawright.move_velocity(0);
-	pros::delay(1000);
+  // 1 red front
+  // 2 blue front
+  // 3 red back
+  // 4 blue back
+  // 5 skills 1
+  // 6 none
 
-	leftfront.move_velocity(-100);
-	leftback.move_velocity(-100);
-	rightfront.move_velocity(-100);
-	rightback.move_velocity(-100);
-	pros::delay(1200);
+  switch(auton_sel) // assign value to side and isfront based on the auto selection button pressed
+  {
+    case 1:
+    {
+      side    = red;
+      isfront = front;
+      break;
+    }
+    case 2:
+    {
+      side    = blue;
+      isfront = front;
+      break;
+    }
+    case 3:
+    {
+      side    = red;
+      isfront = back;
+      break;
+    }
+    case 4:
+    {
+      side    = blue;
+      isfront = back;
+      break;
+    }
+    case 5:
+    {
+      side    = red;
+      isfront = front;
+      break;
+    }
+    case 6:
+    {
+      side    = blue;
+      isfront = front;
+      break;
+    }
+    default:
+    {
+      break;
+    }
+  }
 
-	leftfront.move_velocity(0);
-	leftback.move_velocity(0);
-	rightfront.move_velocity(0);
-	rightback.move_velocity(0);
-	pros::delay(1500);
-}
+  if (auton_sel>=1 && auton_sel <=4 ) {
+    switch (isfront) {
+      case front : { //////////////////////// front ///////////////////////////
+        if (side==blue) {
+          master.print   (0, 0, "bluefront: %d", auton_sel, blue, isfront);
 
+          master.clear();
+
+        	leftfront.move_velocity(100);      //move forward
+        	leftback.move_velocity(100);
+        	rightfront.move_velocity(100);
+        	rightback.move_velocity(100);
+        	pros::delay(2000);
+
+        	leftfront.move_velocity(-100);     //move backwards
+        	leftback.move_velocity(-100);
+        	rightfront.move_velocity(-100);
+        	rightback.move_velocity(-100);
+        	pros::delay(1200);
+
+        	leftfront.move_velocity(0);      //stop
+        	leftback.move_velocity(0);
+        	rightfront.move_velocity(0);
+        	rightback.move_velocity(0);
+        	pros::delay(1500);
+
+        } else {
+           master.print  (0, 0, "redfront:  %d", auton_sel);
+
+           master.clear();
+
+          leftfront.move_velocity(100);     //move forward
+          leftback.move_velocity(100);
+          rightfront.move_velocity(100);
+          rightback.move_velocity(100);
+          pros::delay(2000);
+
+          leftfront.move_velocity(-100);      //move backwards
+          leftback.move_velocity(-100);
+          rightfront.move_velocity(-100);
+          rightback.move_velocity(-100);
+          pros::delay(1200);
+
+          leftfront.move_velocity(0);     //stop
+          leftback.move_velocity(0);
+          rightfront.move_velocity(0);
+          rightback.move_velocity(0);
+          pros::delay(1500);
+        }
+
+        if (auton_sel>=2 && auton_sel <=3 ) {
+          switch (isfront) {
+            case front : { //////////////////////// front ///////////////////////////
+              if (side==blue) {
+                master.print   (0, 0, "blueback: %d", auton_sel, blue, isfront);
+
+                master.clear();
+
+                leftfront.move_velocity(100);     //move forwards
+                leftback.move_velocity(100);
+                rightfront.move_velocity(100);
+                rightback.move_velocity(100);
+                pros::delay(4000);
+
+                clawleft.move_velocity(100);      //intake cubes
+                clawright.move_velocity(100);
+                pros::delay(5000);
+
+                leftfront.move_velocity(-100);      //turn 90 left
+                leftback.move_velocity(-100);
+                rightfront.move_velocity(100);
+                rightback.move_velocity(100);
+                pros::delay(750);
+
+                leftfront.move_velocity(-100);      //stride left
+                leftback.move_velocity(100);
+                rightfront.move_velocity(100);
+                rightback.move_velocity(-100);
+                pros::delay(4000);
+
+                leftfront.move_velocity(0);     //stop moving
+                leftback.move_velocity(0);
+                rightfront.move_velocity(0);
+                rightback.move_velocity(0);
+                pros::delay(100);
+
+                clawleft.move_velocity(-50);      //intake out
+                clawright.move_velocity(-50);
+                pros::delay(500);
+
+                clawleft.move_velocity(0);      //intake stop
+                clawright.move_velocity(0);
+                pros::delay(100);
+
+                stacker.move_velocity(100);     //stacker out
+                pros::delay(1000);
+
+                stacker.move_velocity(-100);      //stacker in
+                pros::delay(1000);
+
+                stacker.move_velocity(0);     //stacker stop
+                pros::delay(100);
+
+                leftfront.move_velocity(-100);      //move backwards
+                leftback.move_velocity(-100);
+                rightfront.move_velocity(-100);
+                rightback.move_velocity(-100);
+                pros::delay(1000);
+
+                leftfront.move_velocity(0);     //stop moving
+                leftback.move_velocity(0);
+                rightfront.move_velocity(0);
+                rightback.move_velocity(0);
+                pros::delay(100);
+
+              } else {
+                 master.print  (0, 0, "redback:  %d", auton_sel);
+
+                 master.clear();
+
+                 leftfront.move_velocity(100);      //move forwards
+                 leftback.move_velocity(100);
+                 rightfront.move_velocity(100);
+                 rightback.move_velocity(100);
+                 pros::delay(4000);
+
+                 clawleft.move_velocity(100);     //intake cubes
+                 clawright.move_velocity(100);
+                 pros::delay(5000);
+
+                 leftfront.move_velocity(100);     //turn 90 right
+                 leftback.move_velocity(100);
+                 rightfront.move_velocity(-100);
+                 rightback.move_velocity(-100);
+                 pros::delay(750);
+
+                 leftfront.move_velocity(-100);     //stride right
+                 leftback.move_velocity(100);
+                 rightfront.move_velocity(100);
+                 rightback.move_velocity(-100);
+                 pros::delay(4000);
+
+                 leftfront.move_velocity(0);     //stop moving
+                 leftback.move_velocity(0);
+                 rightfront.move_velocity(0);
+                 rightback.move_velocity(0);
+                 pros::delay(100);
+
+                 clawleft.move_velocity(-50);     //intake out
+                 clawright.move_velocity(-50);
+                 pros::delay(500);
+
+                 clawleft.move_velocity(0);     //intake stop
+                 clawright.move_velocity(0);
+                 pros::delay(100);
+
+                 stacker.move_velocity(100);     //stacker out
+                 pros::delay(1000);
+
+                 stacker.move_velocity(-100);     //stacker in
+                 pros::delay(1000);
+
+                 stacker.move_velocity(0);     //stacker stop
+                 pros::delay(100);
+
+                 leftfront.move_velocity(-100);     //move backwards
+                 leftback.move_velocity(-100);
+                 rightfront.move_velocity(-100);
+                 rightback.move_velocity(-100);
+                 pros::delay(1000);
+
+                 leftfront.move_velocity(0);     //stop moving
+                 leftback.move_velocity(0);
+                 rightfront.move_velocity(0);
+                 rightback.move_velocity(0);
+                 pros::delay(100);
+              }
+
+  }}} else if (auton_sel ==5 )
+  {
+    //////////////////////////////// skill 1 //////////////////////////////////
+  } else if (auton_sel == 6)
+  {
+
+  }}}}}
 /**
  * Runs the operator control code. This function will be started in its own task
  * with the default priority and stack size whenever the robot is enabled via
@@ -110,17 +313,13 @@ void autonomous() {
  */
  void opcontrol() {
 
-   char mytext[64];
-
-/*Create a screen*/
+   char mytext[64];       /*Create a screen*/
 lv_obj_t * scr = lv_obj_create(NULL, NULL);
 lv_scr_load(scr);                                  /*Load the screen*/
    lv_obj_clean(lv_scr_act());  // clean screen
   lv_obj_t * title = lv_label_create(lv_scr_act(), NULL);
   lv_label_set_text(title, "Debug");
   lv_obj_align(title, NULL, LV_ALIGN_IN_TOP_MID, 0, 20);  /*Align to the top*/
-
-
 
   /*Create a new label*/
   lv_obj_t * txt = lv_label_create(lv_scr_act(), NULL);
@@ -133,18 +332,17 @@ lv_scr_load(scr);                                  /*Load the screen*/
   lv_obj_align(txt, NULL, LV_ALIGN_IN_TOP_LEFT, 10, 20);      /*Align to center*/
 
 	while (true) {
-
-    // assign value to mytext
-sprintf(mytext, "leftfront: %8.2f\n"
+                                        // assign value to mytext
+sprintf(mytext,
+  "leftfront:  %8.2f\n"
   "rightfront: %8.2f\n"
-  "stacker: %8.2f\n"
-  "arm: %8.2f",
+  "stacker:    %d\n"
+  "arm:        %8.2f",
      leftfront.get_position(),
      rightfront.get_position(),
-  stacker.get_position(),
-  lift.get_position()
-   );
-// print to screen
+     stacker.get_position(),
+     lift.get_position()
+   );                               // print to screen
 lv_label_set_text(txt, mytext);
 
 		leftfront.set_brake_mode  (pros::E_MOTOR_BRAKE_HOLD);
@@ -156,27 +354,21 @@ lv_label_set_text(txt, mytext);
 		lift.set_brake_mode       (pros::E_MOTOR_BRAKE_HOLD);
 		stacker.set_brake_mode    (pros::E_MOTOR_BRAKE_HOLD);
 
-		int y = master.get_analog (ANALOG_LEFT_Y);
-		int x = master.get_analog (ANALOG_LEFT_X);
-		int z = master.get_analog (ANALOG_RIGHT_X);
+int forwardback = master.get_analog (ANALOG_LEFT_Y);
+int stride      = master.get_analog (ANALOG_LEFT_X);
+int turn        = master.get_analog (ANALOG_RIGHT_X);
 
 		// chasis
-		leftfront.move  (y + x + z);
-		leftback.move   (y - x + z);
-		rightfront.move (y - x - z);
-		rightback.move  (y + x - z);
+	leftfront.move  (forwardback + turn + stride);
+	leftback.move   (forwardback + turn - stride);
+  rightfront.move (forwardback - turn - stride);
+  rightback.move  (forwardback - turn + stride);
+
 
 		//lift
-		if(master.get_digital(DIGITAL_L1) )
+		if(master.get_digital(DIGITAL_L1))
 		{
-      if (stacker.get_position()>-300.0) {
-             stacker.move_velocity(-50);
-             //pros::delay(20);
-          } else {
-            stacker.move_velocity(0);
-
 				lift.move_velocity (100);
-      }
 		}
 		else if(master.get_digital(DIGITAL_L2))
 		{
@@ -205,22 +397,18 @@ lv_label_set_text(txt, mytext);
 		}
 
 		//stacker
-		if(master.get_digital(DIGITAL_R2)  )
+		if(master.get_digital(DIGITAL_R2))
 		{
-				stacker.move_velocity (50);
+				stacker.move_velocity (75);
 		}
 		else if(master.get_digital(DIGITAL_R1))
 		{
-				stacker.move_velocity (-50);
+				stacker.move_velocity (-100);
 		}
-		else if (master.get_digital(DIGITAL_L1)==0)
+		else
 		{
 			stacker.move_velocity (0);
 		}
-
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
 
 		pros::delay(20);
 	}
